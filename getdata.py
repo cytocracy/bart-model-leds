2846750131f16f595a30c537add1734c9c649889
cytocracy <theop.parker@gmail.com>
2022-04-29 20:08:30 -0700

    api integration and current stations

---
+++ getdata.py
@@ -0,0 +1,43 @@
+
+import requests
+import json
+
+stationDict = {}
+curr_stations = []
+
+def get_stations():
+    data = requests.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y').json()
+    global stations
+    stations = data["root"]["stations"]["station"]
+    # print(stations)
+    dict_stations()
+
+def dict_stations():
+    global stationDict
+    
+    for x in stations:  
+        stationDict[x["abbr"]] = x
+
+
+def get_curr_stations():
+    data = requests.get('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=ALL&key=MW9S-E7SL-26DU-VV8V&json=y').json()
+    for x in data["root"]["station"]:
+        for i in x["etd"]:
+            for j in i["estimate"]:
+                if(j["minutes"] == "Leaving"):
+                    curr_stations.append(x["name"])
+
+        # lowest = x["etd"][0]
+        # for i in x["etd"]:
+        #     if int(i["estimate"][0]["minutes"]) < int(lowest["estimate"][0]["minutes"]):
+        #         lowest = i
+        # if (int(lowest["estimate"][0]["minutes"]) <= 5):
+        #     curr_stations.append(x["name"])
+
+    # print(json.dumps(data, indent=4))
+
+get_stations()
+#print(json.dumps(stationDict, indent=4))
+get_curr_stations()
+print('\nCurrent stations: \n \n' + curr_stations.__str__() + '\n')
+
