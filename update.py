728437d88596891d7fe42342b67297b973a27714
cytocracy <theop.parker@gmail.com>
2022-05-13 16:04:54 -0700

    print which leds

--- update.py
+++ update.py
@@ -13,6 +13,6 @@ def update():
         id = constants.get_station(x[0], x[2])
         color = constants.get_color(x[1].lower()) 
         pixels[id] = color
-        # print(id, x[1], color)
+        print(id, x[1], color)
 
 update()
