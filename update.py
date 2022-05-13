b863811437e3a315d9b54a83a313f01dba11112a
cytocracy <theop.parker@gmail.com>
2022-05-13 15:53:13 -0700

    get rid of print

--- update.py
+++ update.py
@@ -12,7 +12,7 @@ def update():
     for x in stations:
         id = constants.get_station(x[0], x[2])
         color = constants.get_color(x[1].lower()) 
-        # pixels[id] = color
-        print(id, x[1], color)
+        pixels[id] = color
+        # print(id, x[1], color)
 
 update()
